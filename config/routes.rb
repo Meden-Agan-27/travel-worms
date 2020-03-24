5b3e84865a3f77caa58db209d79057032b72a3ec
Gheorghe Tarcea <tarcea@gmail.com>
2020-03-24 16:33:03 +0100

    some routes added

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :bookshelves do
+    resources :bookshelf_items, only: [:new, :create]
   end
-=======
   resources :books
->>>>>>> master
 end
