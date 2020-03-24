1a3c7aab3a3cd74c4eea7a53d1e90642d2b82ba5
Sander Nobel <sander.nobel@protonmail.com>
2020-03-24 13:12:53 +0100

    added the index page and the show page for books

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+    if current_user
+      @books = Book.where(language: current_user.preferred_language )
+    else
+      @books = Book.all
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+end
