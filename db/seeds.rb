6fac37146eb5fb413bf14d1086a51e2725e36c29
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-03-25 18:22:30 +0100

    added 118 books to our seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ def scrape_books(country)
     url = "https://www.goodreads.com/shelf/show/#{query}"
     html_file = open(url).read
     html_doc = Nokogiri::HTML(html_file)
-    html_doc.search('.elementList').first(2).each do |book|
+    html_doc.search('.elementList').first(30).each do |book|
       title = book.search('a').attribute('title').value
       author = book.search('.authorName').text
       image = book.search('img').attribute('src').value
@@ -22,7 +22,7 @@ def scrape_countries
   url = "https://www.worldometers.info/geography/alphabetical-list-of-countries/"
   html_file = open(url).read
   html_doc = Nokogiri::HTML(html_file)
-  html_doc.search('tr').first(3).each do |element|
+  html_doc.search('tr').first(5).each do |element|
     country = element.search('td:nth-child(2)').text.strip.downcase
       scrape_books(country)
   end
