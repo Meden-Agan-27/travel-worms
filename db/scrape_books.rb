deeca3e08e842b3bcec6d02747279b042f63339a
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-03-25 17:35:56 +0100

    scraping goodreads

--- db/scrape_books.rb
+++ db/scrape_books.rb
@@ -1,22 +1,27 @@
 require 'open-uri'
 require 'nokogiri'
 
-def scrape_books
-  countries = ["DNK", "NOR", "SWE"]
-  languages = ['eng', 'ron', 'ita', 'fra', 'spa', 'deu']
-  countries.each do |country|
-    languages.each do |language|
-      url = "http://www.unesco.org/xtrans/bsresult.aspx?a=&stxt=&sl=&l=ita&c=ROU&pla=&pub=&tr=&e=&udc=&d=&from=1990&to=&tie=a"
-      html_file = open(url).read
-      html_doc = Nokogiri::HTML(html_file)
-      html_doc.search('td.res2').each do |element|
-        author = "#{element.search('.sn_auth_firstname').text.strip} #{element.search('.sn_auth_name').text.strip}"
-        puts author
-        # Book.new(title: "Harold et Maude", genre: "novel", language: "french").save
-      end
-    end
+
+def scrape_books(country)
+  query = country.match?("\s") ? country.gsub("\s", "%20") : country
+  url = "https://www.goodreads.com/shelf/show/#{query}"
+  html_file = open(url).read
+  html_doc = Nokogiri::HTML(html_file)
+  html_doc.search('.elementList').first(2).each do |book|
+    title = book.search('a').attribute('title').value
+    author = book.search('.authorName').text
+    image = book.search('img').attribute('src').value
+    Book.create(title: title, language: country)
   end
 end
 
+def scrape_countries
 
-scrape_books
+  url = "https://www.worldometers.info/geography/alphabetical-list-of-countries/"
+  html_file = open(url).read
+  html_doc = Nokogiri::HTML(html_file)
+  html_doc.search('tr').first(1).each do |element|
+    country = element.search('td:nth-child(2)').text.strip.downcase
+      scrape_books(country)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,74 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 puts "Cleaning DB"
 User.destroy_all
 Book.destroy_all
+def scrape_books(country)
+  query = country.match?(/\s/) ? country.gsub("\s", "%20") : country
+  query = country.match?(/\(([^)]+)\)/) ? country.gsub(/\(([^)]+)\)/, "").strip : country
+  begin
+    url = "https://www.goodreads.com/shelf/show/#{query}"
+    html_file = open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+    html_doc.search('.elementList').first(2).each do |book|
+      title = book.search('a').attribute('title').value
+      author = book.search('.authorName').text
+      image = book.search('img').attribute('src').value
+      Book.create(title: title, language: country)
+    end
+   rescue URI::InvalidURIError
+    puts "Invalid country"
+  end
+end
 
-puts "Creating Users"
-User.new(username: "maddy", password: "password", email: "maddy@book.com", preferred_language: "english").save
-User.new(username: "ghita", password: "password", email: "ghita@book.com", preferred_language: "english").save
-User.new(username: "sano", password: "password", email: "sano@book.com", preferred_language: "english").save
-User.new(username: "rafiki", password: "password", email: "rafiki@book.com", preferred_language: "french").save
-puts "Creating Books"
+def scrape_countries
 
-Book.new(title: "Harold et Maude", genre: "novel", language: "french").save
-Book.new(title: "Ulysses", genre: "novel", language: "english").save
-Book.new(title: "War and Peace", genre: "novel", language: "english").save
-Book.new(title: "Narnia", genre: "fantasy", language: "english").save
-Book.new(title: "Hobbit", genre: "novel", language: "english").save
-Book.new(title: "Poems by Me", genre: "poem", language: "english").save
-Book.new(title: "Hubs and cities in Middle Ages (Europe)", genre: "history", language: "english").save
-puts "Creating Bookshelves"
-Bookshelf.new(name: "my_books", user: User.first).save
-Bookshelf.new(name: "my_books", user: User.last).save
-Bookshelf.new(name: "later", user: User.first).save
-Bookshelf.new(name: "later", user: User.last).save
-Bookshelf.new(name: "my_books", user: User.second).save
-Bookshelf.new(name: "later", user: User.second).save
-puts "Creating Bookshelf_items"
-BookshelfItem.new(bookshelf: Bookshelf.first, book: Book.last).save
-BookshelfItem.new(bookshelf: Bookshelf.first, book: Book.first).save
-BookshelfItem.new(bookshelf: Bookshelf.first, book: Book.second).save
-BookshelfItem.new(bookshelf: Bookshelf.last, book: Book.last).save
-BookshelfItem.new(bookshelf: Bookshelf.last, book: Book.first).save
-BookshelfItem.new(bookshelf: Bookshelf.last, book: Book.second).save
-puts "Creating reviews"
-Review.new(user: User.first, book: Book.first, review_content: "I didn't like this book coz I can't read.", rating: 1).save
-Review.new(user: User.second, book: Book.second, review_content: "I loved this book coz I can't read.", rating: 5).save
-Review.new(user: User.second, book: Book.last, review_content: "It was OK.", rating: 3).save
-Review.new(user: User.second, book: Book.first, review_content: "I hated this book coz I can read.", rating: 1).save
-Review.new(user: User.last, book: Book.last, review_content: "Well, you know", rating: 3).save
-puts "Seeding done..."
-10.times { puts ">" }
-puts "... Congratulations!"
+  url = "https://www.worldometers.info/geography/alphabetical-list-of-countries/"
+  html_file = open(url).read
+  html_doc = Nokogiri::HTML(html_file)
+  html_doc.search('tr').first(3).each do |element|
+    country = element.search('td:nth-child(2)').text.strip.downcase
+      scrape_books(country)
+  end
+end
+
+scrape_countries
+
+
+# puts "Creating Users"
+# User.new(username: "maddy", password: "password", email: "maddy@book.com", preferred_language: "english").save
+# User.new(username: "ghita", password: "password", email: "ghita@book.com", preferred_language: "english").save
+# User.new(username: "sano", password: "password", email: "sano@book.com", preferred_language: "english").save
+# User.new(username: "rafiki", password: "password", email: "rafiki@book.com", preferred_language: "french").save
+# puts "Creating Books"
+
+# Book.new(title: "Harold et Maude", genre: "novel", language: "french").save
+# Book.new(title: "Ulysses", genre: "novel", language: "english").save
+# Book.new(title: "War and Peace", genre: "novel", language: "english").save
+# Book.new(title: "Narnia", genre: "fantasy", language: "english").save
+# Book.new(title: "Hobbit", genre: "novel", language: "english").save
+# Book.new(title: "Poems by Me", genre: "poem", language: "english").save
+# Book.new(title: "Hubs and cities in Middle Ages (Europe)", genre: "history", language: "english").save
+# puts "Creating Bookshelves"
+# Bookshelf.new(name: "my_books", user: User.first).save
+# Bookshelf.new(name: "my_books", user: User.last).save
+# Bookshelf.new(name: "later", user: User.first).save
+# Bookshelf.new(name: "later", user: User.last).save
+# Bookshelf.new(name: "my_books", user: User.second).save
+# Bookshelf.new(name: "later", user: User.second).save
+# puts "Creating Bookshelf_items"
+# BookshelfItem.new(bookshelf: Bookshelf.first, book: Book.last).save
+# BookshelfItem.new(bookshelf: Bookshelf.first, book: Book.first).save
+# BookshelfItem.new(bookshelf: Bookshelf.first, book: Book.second).save
+# BookshelfItem.new(bookshelf: Bookshelf.last, book: Book.last).save
+# BookshelfItem.new(bookshelf: Bookshelf.last, book: Book.first).save
+# BookshelfItem.new(bookshelf: Bookshelf.last, book: Book.second).save
+# puts "Creating reviews"
+# Review.new(user: User.first, book: Book.first, review_content: "I didn't like this book coz I can't read.", rating: 1).save
+# Review.new(user: User.second, book: Book.second, review_content: "I loved this book coz I can't read.", rating: 5).save
+# Review.new(user: User.second, book: Book.last, review_content: "It was OK.", rating: 3).save
+# Review.new(user: User.second, book: Book.first, review_content: "I hated this book coz I can read.", rating: 1).save
+# Review.new(user: User.last, book: Book.last, review_content: "Well, you know", rating: 3).save
+# puts "Seeding done..."
+# 10.times { puts ">" }
+# puts "... Congratulations!"
