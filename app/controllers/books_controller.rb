7c3ebc990f5500bff5d65333079da361b006b40d
Sander Nobel <sander.nobel@protonmail.com>
2020-03-26 11:12:32 +0100

    intermittent checkup in order to create a new branch

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if current_user
