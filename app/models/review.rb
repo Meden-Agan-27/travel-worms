c732ea57d8690e89ba1583b15e041f52d60caf7b
Madeline Andrean <madelineandrean@gmail.com>
2020-03-26 13:55:36 +0100

    average ratings

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,13 @@ class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
   validates_presence_of :review_content, :rating
+
+  def average_rating
+    if
+      @reviews >= 1
+      @reviews = self.class.average(:rating)
+    else
+      return "There are no reviews for this book yet."
+    end
+  end
 end
