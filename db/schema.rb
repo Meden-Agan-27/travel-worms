862fe5d9cc903ec66bf828952a7472822ab03017
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-03-30 13:22:43 +0200

    add description to books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_090104) do
+ActiveRecord::Schema.define(version: 2020_03_30_112001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_090104) do
     t.string "original_title"
     t.string "fra_title"
     t.string "spa_title"
+    t.text "description"
   end
 
   create_table "bookshelf_items", force: :cascade do |t|
