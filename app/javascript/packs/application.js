8db9c4323340446e02cb41475758aae5a2e7e48a
Madeline Andrean <madelineandrean@gmail.com>
2020-03-31 09:51:22 +0200

    show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
-import "bootstrap";
-import { initStarRating } from '../plugins/init_star_rating';
-import { topFunction, scrollFunction } from '../plugins/scroll_to_top_btn';
+// import "bootstrap";
+// import { initStarRating } from '../plugins/init_star_rating';
+// import { topFunction, scrollFunction } from '../plugins/scroll_to_top_btn';
 
-initStarRating();
-topFunction();
-window.onscroll = function() {scrollFunction()};
+// initStarRating();
+// topFunction();
+// window.onscroll = function() {scrollFunction()};
