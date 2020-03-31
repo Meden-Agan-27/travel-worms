472f17c2b2056636d61d5dc4061c6d251bf7a2c0
Madeline Andrean <madelineandrean@gmail.com>
2020-03-31 12:17:52 +0200

    final changes

--- app/javascript/plugins/scroll_to_top_btn.js
+++ app/javascript/plugins/scroll_to_top_btn.js
@@ -1,25 +1,25 @@
-// //Get the button:
-// const mybutton = document.getElementById("myBtn");
+//Get the button:
+const mybutton = document.getElementById("myBtn");
 
-// // add event listener to listen for click.
-// mybutton.addEventListener("click", topFunction);
+// add event listener to listen for click.
+mybutton.addEventListener("click", topFunction);
 
-// // When the user scrolls down 20px from the top of the document, show the button
-
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
+// When the user scrolls down 20px from the top of the document, show the button
 
 
-// export { topFunction, scrollFunction };
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+export { topFunction, scrollFunction };
