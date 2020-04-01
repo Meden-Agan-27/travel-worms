7c2d742d0a2c8fc914688e1b4d4543f723017678
Sander Nobel <sander.nobel@protonmail.com>
2020-04-01 15:15:01 +0200

    finished the routing

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -8,13 +8,10 @@ class FriendshipsController < ApplicationController
 # step 6: if users are friends, bookshelves are visible
 # step 7: see notification when new friendship request comes in
 
-  def new
-    @friendship = Friendship.new
-  end
-
   def create
     @asker_id = current_user.id
-    @receiver_id = params[:id].to_i
+    @receiver_id = params[:receiver_id].to_i
+    @friendship = Friendship.create(asker_id: @asker_id, receiver_id: @receiver_id)
   end
 
   def accept
