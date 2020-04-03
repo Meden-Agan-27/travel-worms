de1f1a2cce0a6c86601986011b9451e9967695ac
Gheorghe Tarcea <tarcea@gmail.com>
2020-04-03 15:44:50 +0200

    bookshelf item card trials in index, with local photos of books

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_03_31_143553) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,8 +46,6 @@ ActiveRecord::Schema.define(version: 2020_03_31_143553) do
     t.string "author"
     t.string "country"
     t.string "original_title"
-    t.string "fra_title"
-    t.string "spa_title"
     t.text "description"
     t.string "isbn"
   end
